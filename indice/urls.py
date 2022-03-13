4d295b1f8d01ec42bf3d1c4e218ac45fbbc07a1b
Gabriel Graterol <ssbpf01@gmail.com>
2022-03-13 19:44:21 -0400

    Se modificaron las urls

--- indice/urls.py
+++ indice/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
                
     path('', inicio, name="inicio"),
     path('otra_vista', otra_vista, name="otra_vista"),
-    path('numero-random/', numero_random),
+    path('numero-random/', numero_random, name="numero_random"),
     path('dame-numero/<int:numero>', numero_del_usuario),
     path('mi-plan/', mi_plantilla, name="mi_plantilla"),  
 ]
